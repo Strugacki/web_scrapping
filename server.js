bc7367a09fc2ee11d9260cc7d49fdb59b5f8fb5c
Strugacki <mhallman94@gmail.com>
2017-01-21 11:55:22 +0100

    pagination added

--- server.js
+++ server.js
@@ -21,53 +21,122 @@ app.get('/',function(req,res){
 app.post('/download', (req, res) => {
   console.log(req.body);
   if(req.body.site == 'britannica'){
-    var url = 'https://www.britannica.com/search?query=' + req.body.type;
+    var linksNumber = req.body.articlesNumber;
+    var pages = [];
+    var page =1;
+    while(linksNumber>0){
+      linksNumber=linksNumber-10;
+      pages.push(page);
+      console.log(page);
+      page++;
+    }
     var links= [];
-    request(url,function(err,response,body){
-      if(!err && response.statusCode == 200){
+    async.forEachOf(pages,function(singlePage,key,callback){
+      var url = 'https://www.britannica.com/search?query=' + req.body.type + '&page=' + singlePage;
+      var i = 1;
+      request(url,function(err,response,body){
+        if(!err && response.statusCode == 200){
 
-        var $ = cheerio.load(body);
-        var i = 1;
-        var onCurrentSite = $('div#sr-content a').length;
-        var div;
-        while(i<=req.body.articlesNumber){
-            //console.log($("ul li.sr-result:nth-child("+ i +")").children().html());
+          var $ = cheerio.load(body);
+          var onCurrentSite = $('div#sr-content a').length;
+          var div;
+          while(i<=10 && i <= req.body.articlesNumber){
+              console.log("*************************************************");
+              url = 'https://www.britannica.com' + $("ul li.sr-result:nth-child("+ i +")").children().children().attr('href');
+              console.log(url);
+              links.push(url);
+              i++;
+          }
+          var k=0;
+          async.forEachOf(links,function(link,key,callback){
+            request(link,function(subErr,subResponse,bodySubResponse){
+              if(!subErr && subResponse.statusCode == 200){
+                var $$ = cheerio.load(bodySubResponse);
+                var content=" ";
+                $$('p','div.md-center-channel').each(function(){
+                  content += $(this).text();
+                });
+                content = content.substr(0,req.body.maxLength);
+                var file = __dirname + '/britannica/'+ req.body.type +'/' + key +'.txt';
+                console.log('saved');
+                fs.writeFileSync(file,content);
+                k++;
+                callback();
+              }
+            });
+          }, function (err) {
+                if (err) console.error(err.message);
+              });
+        }else{
+          console.log(err);
+          console.log(response.statusCode);
+        }
+      });
+    }, function (err) {
+          if (err) console.error(err.message);
+      });
+  }else if(req.body.site == 'scholarpedia'){
+    var linksNumber = req.body.articlesNumber;
+    var pages = [];
+    var page =1;
+    while(linksNumber>0){
+      linksNumber=linksNumber-20;
+      pages.push(page);
+      console.log(page);
+      page++;
+    }
+    var links= [];
+    var offset = 0;
+    async.forEachOf(pages,function(singlePage,key,callback){
+      url = 'http://www.scholarpedia.org/w/index.php?title=Special:Search&limit=20&offset='+offset+'&redirs=0&profile=default&search=' + req.body.type;
+      console.log(url);
+      offset = offset + 20;
+      var i = 1;
+      request(url,function(err,response,body){
+        if(!err && response.statusCode == 200){
+
+          var $ = cheerio.load(body);
+          while(i<=20 && i <= req.body.articlesNumber){
             console.log("*************************************************");
-            url = 'https://www.britannica.com' + $("ul li.sr-result:nth-child("+ i +")").children().children().attr('href');
+            url = 'http://www.scholarpedia.org' + $("ul.mw-search-results li:nth-of-type("+ i +")").children().children().attr('href');
             console.log(url);
             links.push(url);
             i++;
-        }
-        var k=0;
-
-        async.forEachOf(links,function(link,key,callback){
-          request(link,function(subErr,subResponse,bodySubResponse){
-            if(!subErr && subResponse.statusCode == 200){
-              var $$ = cheerio.load(bodySubResponse);
-              var content=" ";
-              $$('p','div.md-center-channel').each(function(){
-                content += $(this).text();
-              });
-              var file = __dirname + '/britannica/'+ req.body.type +'/' + key +'.txt';
-              fs.writeFileSync(file,content);
-              k++;
-              callback();
-            }
-          });
-        }, function (err) {
-              if (err) console.error(err.message);
-              // configs is now a map of JSON data
-              //doSomethingWith(configs);
+          }
+          var k=0;
+          async.forEachOf(links,function(link,key,callback){
+            request(link,function(subErr,subResponse,bodySubResponse){
+              if(!subErr && subResponse.statusCode == 200){
+                var $$ = cheerio.load(bodySubResponse);
+                var content=" ";
+                $$('p','div#mw-content-text').each(function(){
+                  content += $(this).text();
+                });
+                content = content.substr(0,req.body.maxLength)
+                var file = __dirname + '/scholarpedia/'+ req.body.type +'/' + key +'.txt';
+                console.log('saved');
+                fs.writeFileSync(file,content);
+                k++;
+                callback();
+              }
             });
-      }else{
-        console.log(err);
-        console.log(response.statusCode);
-      }
-    });
-  }else if(req.body.site == 'scholarpedia'){
-    var url = 'http://www.scholarpedia.org/w/index.php?search=' + req.body.type;
-    var links= [];
-    request(url,function(err,response,body){
+          }, function (err) {
+                if (err) console.error(err.message);
+              });
+        }else{
+          console.log(err);
+          console.log(response.statusCode);
+        }
+      });
+    }, function (err) {
+          if (err) console.error(err.message);
+      });
+
+
+
+
+
+    /*request(url,function(err,response,body){
       if(!err && response.statusCode == 200){
 
         var $ = cheerio.load(body);
@@ -98,8 +167,46 @@ app.post('/download', (req, res) => {
           });
         }, function (err) {
               if (err) console.error(err.message);
-              // configs is now a map of JSON data
-              //doSomethingWith(configs);
+            });
+      }else{
+        console.log(err);
+        console.log(response.statusCode);
+      }
+    });*/
+  }else if(req.body.site == "sciencedaily"){
+    var url = 'https://www.sciencedaily.com/search/?keyword=' + req.body.type;
+    var links= [];
+    request(url,function(err,response,body){
+      if(!err && response.statusCode == 200){
+
+        var $ = cheerio.load(body);
+        var i = 1;
+        var div;
+        while(i<=req.body.articlesNumber){
+            console.log("*************************************************");
+            url = $("div.expansionArea a:nth-of-type("+ i +")").attr('href');
+            console.log(url);
+            links.push(url);
+            i++;
+        }
+        var k=0;
+
+        async.forEachOf(links,function(link,key,callback){
+          request(link,function(subErr,subResponse,bodySubResponse){
+            if(!subErr && subResponse.statusCode == 200){
+              var $$ = cheerio.load(bodySubResponse);
+              var content=" ";
+              $$('p','div#mw-content-text').each(function(){
+                content += $(this).text();
+              });
+              var file = __dirname + '/sciencedaily/'+ req.body.type +'/' + key +'.txt';
+              fs.writeFileSync(file,content);
+              k++;
+              callback();
+            }
+          });
+        }, function (err) {
+              if (err) console.error(err.message);
             });
       }else{
         console.log(err);
